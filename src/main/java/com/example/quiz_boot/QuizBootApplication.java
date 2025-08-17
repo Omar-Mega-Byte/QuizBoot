77d64dc346c62f320b1ad8670646bb420f53b6d8
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-08-17 21:38:54 +0300

    Add main application and test classes for Quiz Boot project

---
+++ src/main/java/com/example/quiz_boot/QuizBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.quiz_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quiz_boot/QuizBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quiz_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
