d4e2f10a39dae0d7dd4b730f116df5f5d42c98b0
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-08-17 23:08:14 +0300

    Add user, role, and user-role entity classes with JPA annotations

---
+++ src/main/java/com/example/quiz_boot/modules/user/model/Role.java
@@ -0,0 +1,21 @@
+package com.example.quiz_boot.modules.user.model;
+
+import com.example.quiz_boot.shared.Base.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "roles")
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Role extends BaseEntity {
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+}
