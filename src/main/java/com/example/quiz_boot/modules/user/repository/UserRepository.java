e88adccaa38d6f2f9489d01b2b129b22b500cb4a
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-08-20 20:01:22 +0300

    Add UserRepository interface for user data access

---
+++ src/main/java/com/example/quiz_boot/modules/user/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.example.quiz_boot.modules.user.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.quiz_boot.modules.user.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    List<User> findAllByIsActive(boolean isActive);
+
+}
