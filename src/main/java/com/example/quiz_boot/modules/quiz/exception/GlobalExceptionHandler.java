f295b6a1a1125c0497d84e6b5df414683890ea3f
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-08-28 17:20:03 +0300

    Refactor GlobalExceptionHandler to organize exception handling for Quiz, Category, and Question entities

--- src/main/java/com/example/quiz_boot/modules/quiz/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/quiz_boot/modules/quiz/exception/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+  // Quiz Exceptions
   @ExceptionHandler(InvalidQuizException.class)
   public ResponseEntity<String> handleInvalidQuizException(InvalidQuizException ex) {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
@@ -14,6 +16,28 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(QuizNotFoundException.class)
   public ResponseEntity<String> handleQuizNotFoundException(QuizNotFoundException ex) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+  }
+
+  // Category Exceptions
+  @ExceptionHandler(InvalidCategoryException.class)
+  public ResponseEntity<String> handleInvalidCategoryException(InvalidCategoryException ex) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+  }
+
+  @ExceptionHandler(CategoryNotFoundException.class)
+  public ResponseEntity<String> handleCategoryNotFoundException(CategoryNotFoundException ex) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+  }
+
+  // Question Exceptions
+  @ExceptionHandler(InvalidQuestionException.class)
+  public ResponseEntity<String> handleInvalidQuestionException(InvalidQuestionException ex) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+  }
+
+  @ExceptionHandler(QuestionNotFoundException.class)
+  public ResponseEntity<String> handleQuestionNotFoundException(QuestionNotFoundException ex) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
   }
 }
